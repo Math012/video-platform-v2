62a9d5bacfec3a935e07f078c6e5dee9c349cef8
Math <math111.0811@gmail.com>
2024-04-17 15:31:14 -0300

    Criando a interface UserRepository para implementar os métodos do JPA/Hibernate.

---
+++ src/main/java/com/math012/social/video/videoplatformv2/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.math012.social.video.videoplatformv2.repository;
+
+import com.math012.social.video.videoplatformv2.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UUID, UserModel> {
+}

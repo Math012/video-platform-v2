00ad09945c829cc3381bf2597b73edf230f7b881
Math <math111.0811@gmail.com>
2024-04-17 15:26:21 -0300

    Initial commit

---
+++ src/main/java/com/math012/social/video/videoplatformv2/VideoPlatformV2Application.java
@@ -0,0 +1,13 @@
+package com.math012.social.video.videoplatformv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoPlatformV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoPlatformV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/math012/social/video/videoplatformv2/VideoPlatformV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math012.social.video.videoplatformv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoPlatformV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

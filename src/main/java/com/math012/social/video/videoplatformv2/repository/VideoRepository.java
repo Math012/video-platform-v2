b493c92abf8a38fd63041aa6e9b521716593a3f5
Math <math111.0811@gmail.com>
2024-04-17 15:31:44 -0300

    Criando a interface VideoRepository para implementar os métodos do JPA/Hibernate.

---
+++ src/main/java/com/math012/social/video/videoplatformv2/repository/VideoRepository.java
@@ -0,0 +1,11 @@
+package com.math012.social.video.videoplatformv2.repository;
+
+import com.math012.social.video.videoplatformv2.model.VideoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface VideoRepository extends JpaRepository<UUID, VideoModel> {
+}

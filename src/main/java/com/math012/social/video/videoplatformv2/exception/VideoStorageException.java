53c0d86625aa922bd1b95895857f331d1dbe178d
Math012 <math111.0811@gmail.com>
2024-04-24 14:50:56 -0300

    implementando a excecao VideoStorageException

---
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/VideoStorageException.java
@@ -0,0 +1,7 @@
+package com.math012.social.video.videoplatformv2.exception;
+
+public class VideoStorageException extends RuntimeException{
+    public VideoStorageException(String err){
+        super(err);
+    }
+}

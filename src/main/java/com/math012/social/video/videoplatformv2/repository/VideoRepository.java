0b4deb2cd3d2bd3c3b1423d9c5baabd701fefe1b
Math012 <math111.0811@gmail.com>
2024-04-25 17:45:13 -0300

    Criando uma custom query para retornar todos os videos de um usuario especifico

--- src/main/java/com/math012/social/video/videoplatformv2/repository/VideoRepository.java
+++ src/main/java/com/math012/social/video/videoplatformv2/repository/VideoRepository.java
@@ -1,11 +1,20 @@
 package com.math012.social.video.videoplatformv2.repository;
 
+import com.math012.social.video.videoplatformv2.model.UserModel;
 import com.math012.social.video.videoplatformv2.model.VideoModel;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface VideoRepository extends JpaRepository<VideoModel, UUID> {
+
+    @Transactional
+    @Query("SELECT video FROM VideoModel video WHERE video.user=:user")
+    List<VideoModel> findAllVideosByUsername(@Param("user")UserModel userModel);
 }

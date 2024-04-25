1ea1a7a71e3b5cdc0a8513bc81c123655aa55aeb
Math012 <math111.0811@gmail.com>
2024-04-25 18:00:21 -0300

    Adicionando a excecao VideoNotFoundException

---
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/VideoNotFoundException.java
@@ -0,0 +1,8 @@
+package com.math012.social.video.videoplatformv2.exception;
+
+public class VideoNotFoundException extends RuntimeException{
+
+    public VideoNotFoundException(String err){
+        super(err);
+    }
+}

74aad5025e9a5fb5e35c068aeb10eafb280efb0b
Math012 <math111.0811@gmail.com>
2024-04-25 17:03:32 -0300

    Adicionando uma query case-insensitive para buscar o username do usuario

--- src/main/java/com/math012/social/video/videoplatformv2/repository/UserRepository.java
+++ src/main/java/com/math012/social/video/videoplatformv2/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.math012.social.video.videoplatformv2.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    Optional<UserModel> findByUsername(String username);
 }

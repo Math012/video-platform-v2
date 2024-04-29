00000ba4f6918c92bdd12c973fd7aefe3b5da5d9
Math012 <math111.0811@gmail.com>
2024-04-29 18:21:47 -0300

    Criando a TokenProviderException

---
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/TokenProviderException.java
@@ -0,0 +1,8 @@
+package com.math012.social.video.videoplatformv2.exception;
+
+public class TokenProviderException extends RuntKimeException{
+
+    TokenProviderException(String err){
+        super(err);
+    }
+}

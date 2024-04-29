726f020ddd64452eb9197d3d84e63261551a02fe
Math012 <math111.0811@gmail.com>
2024-04-29 18:02:12 -0300

    Implementando a interface UserDetails no UserModel.java

--- src/main/java/com/math012/social/video/videoplatformv2/model/UserModel.java
+++ src/main/java/com/math012/social/video/videoplatformv2/model/UserModel.java
@@ -2,13 +2,16 @@ package com.math012.social.video.videoplatformv2.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
 @Entity
-public class UserModel {
+public class UserModel implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -60,6 +63,31 @@ public class UserModel {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public String getUsername() {

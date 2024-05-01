fc5ae25d898dc2d7517e48a7c77a5a476bbe0906
Math012 <math111.0811@gmail.com>
2024-05-01 17:26:43 -0300

    Criando SecurityConfigException e SecurityAuthenticationException

---
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/SecurityAuthenticationException.java
@@ -0,0 +1,8 @@
+package com.math012.social.video.videoplatformv2.exception;
+
+public class SecurityAuthenticationException extends RuntimeException{
+
+    public SecurityAuthenticationException(String err){
+        super(err);
+    }
+}
---
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/SecurityConfigException.java
@@ -0,0 +1,8 @@
+package com.math012.social.video.videoplatformv2.exception;
+
+public class SecurityConfigException extends RuntimeException{
+
+    public SecurityConfigException(String err){
+        super(err);
+    }
+}

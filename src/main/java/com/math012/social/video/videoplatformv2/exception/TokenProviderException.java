b60762959060b6678f3f5da6b7325b8052e5fca0
Math012 <math111.0811@gmail.com>
2024-05-01 17:18:33 -0300

    Adicionando public no construtor TokenProviderException

--- src/main/java/com/math012/social/video/videoplatformv2/exception/TokenProviderException.java
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/TokenProviderException.java
@@ -2,7 +2,7 @@ package com.math012.social.video.videoplatformv2.exception;
 
 public class TokenProviderException extends RuntimeException{
 
-    TokenProviderException(String err){
+    public TokenProviderException(String err){
         super(err);
     }
 }

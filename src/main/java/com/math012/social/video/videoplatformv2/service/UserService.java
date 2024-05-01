0f24fbb8f4e0bf598b94705a0199d86ff897c2c8
Math012 <math111.0811@gmail.com>
2024-05-01 17:28:13 -0300

    Implementando a logica de registro e login

--- src/main/java/com/math012/social/video/videoplatformv2/service/UserService.java
+++ src/main/java/com/math012/social/video/videoplatformv2/service/UserService.java
@@ -1,7 +1,66 @@
 package com.math012.social.video.videoplatformv2.service;
 
+import com.math012.social.video.videoplatformv2.DTO.RegisterDTO;
+import com.math012.social.video.videoplatformv2.DTO.UserDTO;
+import com.math012.social.video.videoplatformv2.config.security.TokenProvider;
+import com.math012.social.video.videoplatformv2.exception.SecurityAuthenticationException;
+import com.math012.social.video.videoplatformv2.model.UserModel;
+import com.math012.social.video.videoplatformv2.repository.UserRepository;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow();
+    }
+
+    public ResponseEntity<String> login(@RequestBody @Valid UserDTO userDTO){
+        authenticationManager = context.getBean(AuthenticationManager.class);
+        var authenticate = new UsernamePasswordAuthenticationToken(userDTO.username(),userDTO.password());
+        var authenticated = this.authenticationManager.authenticate(authenticate);
+        var token = tokenProvider.tokenProvider((UserModel) authenticated.getPrincipal());
+        return ResponseEntity.ok(token);
+    }
+
+    public ResponseEntity<String> register(@RequestBody @Valid RegisterDTO registerDTO){
+        if (userRepository.findByUsername(registerDTO.username()).isPresent()) throw new SecurityAuthenticationException("This username: " + registerDTO.username() + " already registered");
+        String encryptedPassword = new BCryptPasswordEncoder().encode(registerDTO.password());
+        var newUser = new UserModel(registerDTO.fullName(),registerDTO.lastName(),registerDTO.username(),encryptedPassword);
+        userRepository.save(newUser);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
 }

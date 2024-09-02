9a2db1bfb04772532d56d748aadd3257a5003f27
Math012 <math111.0811@gmail.com>
2024-09-02 11:35:54 -0300

    adicionando try-catch ao UserService.java

--- src/main/java/com/math012/social/video/videoplatformv2/service/UserService.java
+++ src/main/java/com/math012/social/video/videoplatformv2/service/UserService.java
@@ -3,11 +3,15 @@ package com.math012.social.video.videoplatformv2.service;
 import com.math012.social.video.videoplatformv2.DTO.RegisterDTO;
 import com.math012.social.video.videoplatformv2.DTO.UserDTO;
 import com.math012.social.video.videoplatformv2.config.security.TokenProvider;
+import com.math012.social.video.videoplatformv2.exception.RegisterUserException;
 import com.math012.social.video.videoplatformv2.exception.SecurityAuthenticationException;
+import com.math012.social.video.videoplatformv2.exception.TokenProviderException;
+import com.math012.social.video.videoplatformv2.exception.UserNotFoundException;
 import com.math012.social.video.videoplatformv2.model.UserModel;
 import com.math012.social.video.videoplatformv2.repository.UserRepository;
 
 import jakarta.validation.Valid;
+import jdk.swing.interop.SwingInterOpUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
@@ -23,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Objects;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -44,23 +49,32 @@ public class UserService implements UserDetailsService {
     }
 
     public ResponseEntity<String> login(@RequestBody @Valid UserDTO userDTO){
-        authenticationManager = context.getBean(AuthenticationManager.class);
-        var authenticate = new UsernamePasswordAuthenticationToken(userDTO.username(),userDTO.password());
-        var authenticated = this.authenticationManager.authenticate(authenticate);
-        var token = tokenProvider.tokenProvider((UserModel) authenticated.getPrincipal());
-        return ResponseEntity.ok(token);
+        try {
+            authenticationManager = context.getBean(AuthenticationManager.class);
+            var authenticate = new UsernamePasswordAuthenticationToken(userDTO.username(),userDTO.password());
+            var authenticated = this.authenticationManager.authenticate(authenticate);
+            var token = tokenProvider.tokenProvider((UserModel) authenticated.getPrincipal());
+            return ResponseEntity.ok(token);
+        }catch (Exception e){
+            throw new UserNotFoundException("Your credentials are not valid, try again");
+        }
+
     }
 
     public ResponseEntity<String> register(@RequestBody @Valid RegisterDTO registerDTO){
         if (userRepository.findByUsername(registerDTO.username()).isPresent()) throw new SecurityAuthenticationException("This username: " + registerDTO.username() + " already registered");
-        String encryptedPassword = new BCryptPasswordEncoder().encode(registerDTO.password());
-        var newUser = new UserModel(registerDTO.fullName(),registerDTO.lastName(),registerDTO.username(),encryptedPassword);
-        userRepository.save(newUser);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(newUser.getId())
-                .toUri();
-        return ResponseEntity.created(location).build();
+        try {
+            String encryptedPassword = new BCryptPasswordEncoder().encode(registerDTO.password());
+            var newUser = new UserModel(registerDTO.fullName(),registerDTO.lastName(),registerDTO.username(),encryptedPassword);
+            userRepository.save(newUser);
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(newUser.getId())
+                    .toUri();
+            return ResponseEntity.created(location).build();
+        }catch (Exception e){
+            throw new RegisterUserException("Your account has not been registered, please try again");
+        }
     }
 }

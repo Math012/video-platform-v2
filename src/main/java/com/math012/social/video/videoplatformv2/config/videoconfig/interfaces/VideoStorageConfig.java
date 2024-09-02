4029ce232bdf7f64df09322b8f358f9b4084325e
Math012 <math111.0811@gmail.com>
2024-09-02 11:38:46 -0300

    adicionando o metodo loadFile na interface VideoStorageConfig

--- src/main/java/com/math012/social/video/videoplatformv2/config/videoconfig/interfaces/VideoStorageConfig.java
+++ src/main/java/com/math012/social/video/videoplatformv2/config/videoconfig/interfaces/VideoStorageConfig.java
@@ -1,8 +1,11 @@
 package com.math012.social.video.videoplatformv2.config.videoconfig.interfaces;
 
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface VideoStorageConfig {
 
     public String saveFile(MultipartFile file, String username);
+
+    public Resource loadFile(String path, String username);
 }

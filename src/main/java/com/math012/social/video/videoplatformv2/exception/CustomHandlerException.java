5978e96f5ad9253194b3470bdb4e26cf6f742484
Math <math111.0811@gmail.com>
2024-10-21 19:03:13 -0300

    adicionando as exceções personalizadas: UserDescriptionException, NoUsersException, PhotoProfileException, ThumbnailException CommentException, InvalidFieldsException

--- src/main/java/com/math012/social/video/videoplatformv2/exception/CustomHandlerException.java
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/CustomHandlerException.java
@@ -4,14 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 
 @ControllerAdvice
-@RestController
 public class CustomHandlerException extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
@@ -58,6 +56,42 @@ public class CustomHandlerException extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(LoadVideoException.class)
     public ResponseEntity<StructException> handlerLoadVideoException(Exception e, WebRequest request){
+        var exception = new StructException(new Date(), e.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exception, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserDescriptionException.class)
+    public ResponseEntity<StructException> handlerUserDescriptionException(Exception e, WebRequest request){
+        var exception = new StructException(new Date(), e.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoUsersException.class)
+    public ResponseEntity<StructException> handlerNoUsersException(Exception e, WebRequest request){
+        var exception = new StructException(new Date(), e.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(PhotoProfileException.class)
+    public ResponseEntity<StructException> handlerPhotoProfileException(Exception e, WebRequest request){
+        var exception = new StructException(new Date(), e.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ThumbnailException.class)
+    public ResponseEntity<StructException> handlerThumbnailException(Exception e, WebRequest request){
+        var exception = new StructException(new Date(), e.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CommentException.class)
+    public ResponseEntity<StructException> handlerCommentException(Exception e, WebRequest request){
+        var exception = new StructException(new Date(), e.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidFieldsException.class)
+    public ResponseEntity<StructException> handlerInvalidFieldsException(Exception e, WebRequest request){
         var exception = new StructException(new Date(), e.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
     }

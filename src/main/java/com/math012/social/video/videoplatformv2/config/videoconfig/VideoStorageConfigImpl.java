76012214852a641adf5fd0a8e51595170c5ed9ee
Math <math111.0811@gmail.com>
2024-10-21 19:12:07 -0300

    adicionando aos métodos "saveFile" e "loadFile" a verificação de valores vazios ou nulos.

--- src/main/java/com/math012/social/video/videoplatformv2/config/videoconfig/VideoStorageConfigImpl.java
+++ src/main/java/com/math012/social/video/videoplatformv2/config/videoconfig/VideoStorageConfigImpl.java
@@ -1,8 +1,7 @@
 package com.math012.social.video.videoplatformv2.config.videoconfig;
 
-import com.math012.social.video.videoplatformv2.config.videoconfig.interfaces.VideoStorageConfig;
+import com.math012.social.video.videoplatformv2.config.interfaces.StorageConfig;
 import com.math012.social.video.videoplatformv2.exception.LoadVideoException;
-import com.math012.social.video.videoplatformv2.exception.TokenProviderException;
 import com.math012.social.video.videoplatformv2.exception.UserNotFoundException;
 import com.math012.social.video.videoplatformv2.exception.VideoStorageException;
 import com.math012.social.video.videoplatformv2.repository.UserRepository;
@@ -18,16 +17,11 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-
-/*
-        A ideia inicial é criar um diretorio no disco local C para salvar todos os videos.
-        O caminho foi definido através do application.properties.
- */
-
+import java.util.Objects;
 
 @Configuration
 @ConfigurationProperties(prefix = "file")
-public class VideoStorageConfigImpl implements VideoStorageConfig {
+public class VideoStorageConfigImpl implements StorageConfig {
 
     @Value("${file.upload-dir:dir}")
     private  String pathDir = "dir";
@@ -41,24 +35,26 @@ public class VideoStorageConfigImpl implements VideoStorageConfig {
 
     @Override
     public String saveFile(MultipartFile file, String username) {
-        String filename = StringUtils.cleanPath(file.getOriginalFilename());
-        Path path = Path.of(this.pathDir);
-
         if (userRepository.findByUsername(username).isEmpty()) throw new UserNotFoundException("This username: " + username + ", not found");
+        if (file == null) throw new VideoStorageException("The video was not saved, please try again");
+
+        String filename = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+        Path path = Path.of(this.pathDir);
         createFolder(path,"\\",username);
         Path pathFolder = Path.of(path+"\\"+username);
-        if (!filename.contains(".mp4")) throw new VideoStorageException("Upload only videos");
+        if (!filename.contains(".mp4")) throw new VideoStorageException("Please, send mp4 format only");
         try {
             Path location = pathFolder.resolve(filename);
             Files.copy(file.getInputStream(),location, StandardCopyOption.REPLACE_EXISTING);
             return filename;
         }catch (Exception e){
-            throw new VideoStorageException("The video was not saved, please try again");
+            return e.getMessage();
         }
     }
 
     @Override
     public Resource loadFile(String path, String username) {
+        if (username == null) throw new UserNotFoundException("username invalid, try again");
         try {
             Path pathFile = Path.of(pathDir);
             Path locationPath = Path.of(pathFile+"\\"+username);
@@ -68,7 +64,6 @@ public class VideoStorageConfigImpl implements VideoStorageConfig {
         }catch (Exception e){
             throw new LoadVideoException("Download URL was not found");
         }
-
     }
 
     private void createFolder(Path path, String limiter, String username) {

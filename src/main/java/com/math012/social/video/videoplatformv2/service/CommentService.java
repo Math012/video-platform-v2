83bfbd37dce20e7fb771c07fcb6c7d738be81148
Math <math111.0811@gmail.com>
2024-10-21 18:58:53 -0300

    adicionando a verificação dos parâmetros dos métodos e criando o método deleteCommentById

--- src/main/java/com/math012/social/video/videoplatformv2/service/CommentService.java
+++ src/main/java/com/math012/social/video/videoplatformv2/service/CommentService.java
@@ -1,11 +1,14 @@
 package com.math012.social.video.videoplatformv2.service;
 
+import com.math012.social.video.videoplatformv2.exception.CommentException;
+import com.math012.social.video.videoplatformv2.exception.UserNotFoundException;
+import com.math012.social.video.videoplatformv2.exception.VideoNotFoundException;
 import com.math012.social.video.videoplatformv2.model.CommentModel;
-import com.math012.social.video.videoplatformv2.model.VideoModel;
 import com.math012.social.video.videoplatformv2.repository.CommentRepository;
 import com.math012.social.video.videoplatformv2.repository.UserRepository;
 import com.math012.social.video.videoplatformv2.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,6 +28,13 @@ public class CommentService {
 
 
     public CommentModel postComment(String username, UUID idVideo, String textContent){
+        if (username == null) throw new UserNotFoundException("The username is null, try again");
+        if (idVideo == null) throw new VideoNotFoundException("The video id is null, try again");
+        if (textContent == null) throw new CommentException("The text content is null, try again");
+
+        if (userRepository.findByUsername(username).isEmpty()) throw new UserNotFoundException("The username " + username+" not found, try again");
+        if (videoRepository.findById(idVideo).isEmpty()) throw new VideoNotFoundException("The id " + idVideo+" not found, try again");
+
         var user = userRepository.findByUsername(username).get();
         var video = videoRepository.findById(idVideo).get();
         var comment = new CommentModel(textContent,user,video);
@@ -33,7 +43,19 @@ public class CommentService {
     }
 
     public List<CommentModel> findAllCommentsByVideo(UUID videoId){
+        if (videoId == null) throw new VideoNotFoundException("The video id is null, try again");
+        if (videoRepository.findById(videoId).isEmpty()) throw new VideoNotFoundException("The video id " + videoId + " not found");
         var video = videoRepository.findById(videoId).get();
         return commentRepository.findAllCommentByVideo(video);
+    }
+
+    public void deleteCommentById(UUID id){
+        if (commentRepository.findById(id).isEmpty()) throw new CommentException("The UUID of comment is not valid, please try again");
+        commentRepository.deleteById(id);
+    }
+
+    public void deleteCommentByVideoId(UUID id){
+        if (videoRepository.findById(id).isEmpty()) throw new VideoNotFoundException("The UUID of video is not valid, please try again");
+        commentRepository.deleteCommentByVideoId(id);
     }
 }

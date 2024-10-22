63e8f71b8455ca29f26119be3a7fbddcb59311c9
Math <math111.0811@gmail.com>
2024-10-22 12:24:13 -0300

    criando a exceção personalizada CommentException.

---
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/CommentException.java
@@ -0,0 +1,7 @@
+package com.math012.social.video.videoplatformv2.exception;
+
+public class CommentException extends RuntimeException{
+    public CommentException(String err){
+        super(err);
+    }
+}

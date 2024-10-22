4f0c8bdac26290a1334eb93ef3490fd84ec7b91c
Math <math111.0811@gmail.com>
2024-10-22 12:39:02 -0300

    exceção personalizada ThumbnailException para verificar o envio de thumbnails.

---
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/ThumbnailException.java
@@ -0,0 +1,7 @@
+package com.math012.social.video.videoplatformv2.exception;
+
+public class ThumbnailException extends RuntimeException{
+    public ThumbnailException(String err){
+        super(err);
+    }
+}

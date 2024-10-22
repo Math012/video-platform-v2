0b844d9ec2d95709ce6a366640f9b2984d82bfce
Math <math111.0811@gmail.com>
2024-10-22 12:36:53 -0300

    exceção personalizada NoUsersException para verificar o registro de usuários

---
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/NoUsersException.java
@@ -0,0 +1,7 @@
+package com.math012.social.video.videoplatformv2.exception;
+
+public class NoUsersException  extends RuntimeException{
+    public NoUsersException(String err){
+        super(err);
+    }
+}

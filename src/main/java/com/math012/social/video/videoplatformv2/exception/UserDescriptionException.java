f17db875b57a9774711dfee75868bbf1140be5f3
Math <math111.0811@gmail.com>
2024-10-22 12:39:47 -0300

    exceção personalizada UserDescriptionException para verificar a descrição enviada pelo usuário.

---
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/UserDescriptionException.java
@@ -0,0 +1,9 @@
+package com.math012.social.video.videoplatformv2.exception;
+
+
+public class UserDescriptionException extends RuntimeException{
+
+    public UserDescriptionException(String err){
+        super(err);
+    }
+}

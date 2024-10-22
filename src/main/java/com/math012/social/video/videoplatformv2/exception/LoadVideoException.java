fbd6d279c586909b7e90a317eb0db5912e38831c
Math <math111.0811@gmail.com>
2024-10-22 12:35:10 -0300

    exceção personalizada LoadVideoException para verificar o carregamento de vídeos

---
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/LoadVideoException.java
@@ -0,0 +1,7 @@
+package com.math012.social.video.videoplatformv2.exception;
+
+public class LoadVideoException  extends RuntimeException{
+    public LoadVideoException(String err){
+        super(err);
+    }
+}

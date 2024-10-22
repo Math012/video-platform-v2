0c2b28ca890b0dffbb4ae8355c228ebe94c1ff3c
Math <math111.0811@gmail.com>
2024-10-22 12:38:38 -0300

    exceção personalizada PhotoProfileException para verificar o envio de fotos de perfil

---
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/PhotoProfileException.java
@@ -0,0 +1,7 @@
+package com.math012.social.video.videoplatformv2.exception;
+
+public class PhotoProfileException extends RuntimeException{
+    public PhotoProfileException(String err){
+        super(err);
+    }
+}

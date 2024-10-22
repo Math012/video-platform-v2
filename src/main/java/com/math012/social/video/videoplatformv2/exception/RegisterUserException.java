2fe4d676f7dc3f23b115162f6b08c965f025be16
Math <math111.0811@gmail.com>
2024-10-22 12:37:20 -0300

    exceção personalizada RegisterUserException para verificar o cadastro de usuários

---
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/RegisterUserException.java
@@ -0,0 +1,8 @@
+package com.math012.social.video.videoplatformv2.exception;
+
+public class RegisterUserException extends RuntimeException{
+
+    public RegisterUserException(String err){
+        super(err);
+    }
+}

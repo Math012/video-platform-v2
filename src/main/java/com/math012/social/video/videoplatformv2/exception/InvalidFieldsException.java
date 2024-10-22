3a100fae2991436ade1eb2949c55833608aed5d1
Math <math111.0811@gmail.com>
2024-10-22 12:31:48 -0300

    exceção personalizada InvalidFieldsException para a validação métodos.

---
+++ src/main/java/com/math012/social/video/videoplatformv2/exception/InvalidFieldsException.java
@@ -0,0 +1,7 @@
+package com.math012.social.video.videoplatformv2.exception;
+
+public class InvalidFieldsException extends RuntimeException{
+    public InvalidFieldsException(String err){
+        super(err);
+    }
+}

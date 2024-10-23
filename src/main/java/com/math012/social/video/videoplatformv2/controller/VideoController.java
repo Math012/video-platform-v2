d6725106a1aee0bebcb85f5e1945592b8a882485
Math <math111.0811@gmail.com>
2024-10-23 02:52:49 -0300

    Adicionando o swagger ao VideoController.java e detalhando os endpoints

--- src/main/java/com/math012/social/video/videoplatformv2/controller/VideoController.java
+++ src/main/java/com/math012/social/video/videoplatformv2/controller/VideoController.java
@@ -8,6 +8,9 @@ import com.math012.social.video.videoplatformv2.exception.VideoStorageException;
 import com.math012.social.video.videoplatformv2.model.UserModel;
 import com.math012.social.video.videoplatformv2.model.VideoModel;
 import com.math012.social.video.videoplatformv2.service.VideoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -27,6 +30,7 @@ import java.util.UUID;
 
 @RestController
 @CrossOrigin("*")
+@Tag(name = "video-platform-v2", description = "Controlador para gerenciar os videos do usuário")
 public class VideoController {
 
     @Autowired
@@ -39,26 +43,51 @@ public class VideoController {
     private VideoService videoService;
 
     @GetMapping("video/detail/{id}")
+    @Operation(summary = "Endpoint para retornar um vídeo específico", description = "Retorna um vídeo através do id fornecido")
+    @ApiResponse(responseCode = "200", description = "Vídeo retornado com sucesso")
+    @ApiResponse(responseCode = "404", description = "Vídeo não retornado porque o id do vídeo não foi encontrado")
+    @ApiResponse(responseCode = "404", description = "Vídeo não retornado porque o id do vídeo não foi encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public VideoModel findById(@PathVariable(value = "id")UUID id){
         return videoService.findById(id);
     }
 
     @GetMapping("videos/home")
+    @Operation(summary = "Retorna uma lista de vídeos", description = "Retorna todos os vídeos registrados ")
+    @ApiResponse(responseCode = "200", description = "Vídeos retornados com sucesso")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public List<VideoModel> findAllVideos(){
         return videoService.findAllVideos();
     }
 
     @GetMapping("videos/user/{username}")
+    @Operation(summary = "Retorna todos os vídeos de um usuário", description = "Retorna todos os vídeos de um usuário específico através do seu username")
+    @ApiResponse(responseCode = "200", description = "Vídeos retornados com sucesso")
+    @ApiResponse(responseCode = "400", description = "Vídeos não retornados porque o username esta vazio")
+    @ApiResponse(responseCode = "400", description = "Vídeos não retornados porque o username é nulo")
+    @ApiResponse(responseCode = "404", description = "Vídeos não retornados porque o username não foi encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public List<VideoModel> findAllVideoByUsername(@PathVariable(value = "username")String username){
         return videoService.findAllVideosByUsername(username);
     }
 
     @GetMapping("videos/pageable")
+    @Operation(summary = "Retorna uma lista de vídeos com paginação", description = "Retorna uma lista de vídeos com paginação definida através de parâmetros")
+    @ApiResponse(responseCode = "200", description = "Vídeos retornados com sucesso")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public HashSet<VideoModel> findAllVideosWithPage(Pageable pageable){
         return videoService.findAllVideosWithPages(pageable);
     }
 
     @PostMapping("api/v2/post/video/{username}")
+    @Operation(summary = "Realiza o envio de um vídeo", description = "Recebe um arquivo do tipo MP4 e realiza o upload do arquivo")
+    @ApiResponse(responseCode = "200", description = "Vídeo enviado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Vídeos não enviado porque o username esta vazio")
+    @ApiResponse(responseCode = "400", description = "Vídeos não enviado porque o username é nulo")
+    @ApiResponse(responseCode = "400", description = "Vídeos não enviado porque o arquivo não é MP4")
+    @ApiResponse(responseCode = "403", description = "Vídeos não foi enviado porque a autenticação do usuário falhou")
+    @ApiResponse(responseCode = "404", description = "Vídeos não enviado porque o username não foi encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public VideoModel uploadVideo(MultipartHttpServletRequest request, @PathVariable(value = "username")String username) {
         try {
             var user = videoService.returnUserFromUsername(username);
@@ -98,6 +127,12 @@ public class VideoController {
     }
 
     @GetMapping("api/v2/post/video/download/{username}/{path:.+}")
+    @Operation(summary = "Faz o carregamento do vídeo de um usuário", description = "Realiza a busca do vídeo através de uma url e username")
+    @ApiResponse(responseCode = "200", description = "Vídeo carregado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Vídeos não foi carregado")
+    @ApiResponse(responseCode = "403", description = "Vídeos não foi carregado porque a autenticação do usuário falhou")
+    @ApiResponse(responseCode = "404", description = "Vídeos não carregado porque o username não foi encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<Resource> download(@PathVariable(value = "username")String username, @PathVariable(value = "path") String path, HttpServletRequest request){
         Resource resource = videoStorage.loadFile(path,username);
         String content = null;
@@ -116,6 +151,12 @@ public class VideoController {
     }
 
     @GetMapping("api/v2/post/thumbnail/download/{username}/{path:.+}")
+    @Operation(summary = "Faz o carregamento da thumbnail do usuário", description = "Realiza o carregamento da thumbnail enviada pelo usuário")
+    @ApiResponse(responseCode = "200", description = "Thumbnail carregada com sucesso")
+    @ApiResponse(responseCode = "400", description = "Thumbnail não foi carregada")
+    @ApiResponse(responseCode = "403", description = "Thumbnail não foi carregadoa porque a autenticação do usuário falhou")
+    @ApiResponse(responseCode = "404", description = "Thumbnail não carregada porque o username não foi encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<Resource> downloadThumbnail(@PathVariable(value = "username")String username, @PathVariable(value = "path") String path, HttpServletRequest request){
         Resource resource = thumbnailStorage.loadFile(path, username);
         String content = null;
@@ -129,19 +170,34 @@ public class VideoController {
     }
 
     @DeleteMapping(path = "/api/v2/video/comment/delete/{videoId}")
+    @Operation(summary = "Deleta um vídeo", description = "Deleta o vídeo especificado pelo ID e que tenha comentários")
+    @ApiResponse(responseCode = "200", description = "Vídeo deletado com sucesso")
+    @ApiResponse(responseCode = "403", description = "Vídeo não foi deletado porque a autenticação do usuário falhou")
+    @ApiResponse(responseCode = "404", description = "Vídeo não foi deletado porque o id do vídeo não foi encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<String> deleteVideoWithComment(@PathVariable(value = "videoId")UUID videoId){
         videoService.deleteVideoWithComments(videoId);
         return ResponseEntity.ok("Your video was deleted");
     }
 
     @DeleteMapping(path = "/api/v2/video/{videoId}")
+    @Operation(summary = "Deleta um vídeo", description = "Deleta o vídeo especificado pelo ID")
+    @ApiResponse(responseCode = "200", description = "Vídeo deletado com sucesso")
+    @ApiResponse(responseCode = "403", description = "Vídeo não foi deletado porque a autenticação do usuário falhou")
+    @ApiResponse(responseCode = "404", description = "Vídeo não foi deletado porque o id do vídeo não foi encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<String> deleteVideoWithoutComments(@PathVariable(value = "videoId")UUID videoId){
         videoService.deleteVideoWithoutComments(videoId);
         return ResponseEntity.ok("Your video was deleted");
     }
 
     @GetMapping(path = "/video/username/videos/{videoId}")
-    public String deleteFindUsernameByVideoId(@PathVariable(value = "videoId")UUID videoId){
+    @Operation(summary = "Retorna um username do banco de dados", description = "Retorna um username com base no vídeo id")
+    @ApiResponse(responseCode = "200", description = "Username retornado com sucesso")
+    @ApiResponse(responseCode = "403", description = "Username não foi retornado porque a autenticação do usuário falhou")
+    @ApiResponse(responseCode = "404", description = "Username não foi retornado porque o id do vídeo não foi encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
+    public String findUsernameByVideoId(@PathVariable(value = "videoId")UUID videoId){
         return videoService.findUsernameByVideoId(videoId);
     }
 }
